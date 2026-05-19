322364d436d201a6414b1298c1c9eee22d174ef7
拨开乌云便有阳光 <YBY1306>
2026-05-20 01:40:12 +0800

    watch模块: 聊天列表为空时可长按进行Token登录

--- watch/src/main/java/com/yhchat/canary/MainActivity.kt
+++ watch/src/main/java/com/yhchat/canary/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Modifier
 import com.yhchat.canary.data.di.RepositoryFactory
 import com.yhchat.canary.ui.chat.ChatActivity
 import com.yhchat.canary.ui.conversation.ConversationScreen
+import com.yhchat.canary.ui.wear.login.WearLoginActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -30,7 +31,7 @@ class MainActivity : ComponentActivity() {
                 tokenRepository = tokenRepository,//TODO
                 modifier = Modifier.fillMaxSize(),
                 _to_loginfun_xxxx = {
-//                    startActivity(Intent(this, WearLoginActivity::class.java).apply { })
+                    startActivity(Intent(this, WearLoginActivity::class.java).apply { })
                 }
             )
         }

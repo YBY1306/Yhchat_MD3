0274aa7212912adb15a4f625008d7753614d4132
拨开乌云便有阳光 <YBY1306>
2026-05-28 02:21:32 +0800

    watch模块:

--- watch/src/main/java/com/yhchat/canary/watch/ui/second_floor/SecondFloorDialog.kt
+++ watch/src/main/java/com/yhchat/canary/watch/ui/second_floor/SecondFloorDialog.kt
@@ -1,17 +1,22 @@
 package com.yhchat.canary.watch.ui.second_floor
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.wear.compose.foundation.lazy.TransformingLazyColumn
 import androidx.wear.compose.foundation.lazy.rememberTransformingLazyColumnState
 import androidx.wear.compose.material3.AlertDialog
+import androidx.wear.compose.material3.Button
 import androidx.wear.compose.material3.ButtonDefaults
 import androidx.wear.compose.material3.Dialog
 import androidx.wear.compose.material3.EdgeButton
 import androidx.wear.compose.material3.MaterialTheme
 import androidx.wear.compose.material3.ScreenScaffold
+import androidx.wear.compose.material3.SurfaceTransformation
 import androidx.wear.compose.material3.Text
 import androidx.wear.compose.material3.TextButton
 import androidx.wear.compose.material3.lazy.rememberTransformationSpec
+import androidx.wear.compose.material3.lazy.transformedHeight
 
 @Composable
 fun SecondFloorDialog(onDismissRequest: () -> Unit, onOkClick: () -> Unit) {
@@ -36,7 +41,20 @@ fun SecondFloorDialog(onDismissRequest: () -> Unit, onOkClick: () -> Unit) {
                 }
             },
         ) { contentPadding -> // ScreenScaffold provides default padding; adjust as needed
-            TransformingLazyColumn(contentPadding = contentPadding, state = listState) {}
+            TransformingLazyColumn(contentPadding = contentPadding, state = listState) {
+                item {
+                    Button(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .transformedHeight(this, transformationSpec),
+                        transformation = SurfaceTransformation(transformationSpec),
+                    ) {
+                        Text("Button B")
+                    }
+                }
+
+            }
         }
     }
     // todo 二楼菜单

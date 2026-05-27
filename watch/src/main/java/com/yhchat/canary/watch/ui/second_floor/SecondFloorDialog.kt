9d78822c023b882087d50ad6ff5a756971b8b130
拨开乌云便有阳光 <YBY1306>
2026-05-28 02:14:51 +0800

    watch模块:

--- watch/src/main/java/com/yhchat/canary/watch/ui/second_floor/SecondFloorDialog.kt
+++ watch/src/main/java/com/yhchat/canary/watch/ui/second_floor/SecondFloorDialog.kt
@@ -1,10 +1,17 @@
 package com.yhchat.canary.watch.ui.second_floor
 
 import androidx.compose.runtime.Composable
+import androidx.wear.compose.foundation.lazy.TransformingLazyColumn
+import androidx.wear.compose.foundation.lazy.rememberTransformingLazyColumnState
 import androidx.wear.compose.material3.AlertDialog
+import androidx.wear.compose.material3.ButtonDefaults
 import androidx.wear.compose.material3.Dialog
+import androidx.wear.compose.material3.EdgeButton
+import androidx.wear.compose.material3.MaterialTheme
+import androidx.wear.compose.material3.ScreenScaffold
 import androidx.wear.compose.material3.Text
 import androidx.wear.compose.material3.TextButton
+import androidx.wear.compose.material3.lazy.rememberTransformationSpec
 
 @Composable
 fun SecondFloorDialog(onDismissRequest: () -> Unit, onOkClick: () -> Unit) {
@@ -12,7 +19,25 @@ fun SecondFloorDialog(onDismissRequest: () -> Unit, onOkClick: () -> Unit) {
         onDismissRequest = onDismissRequest,
         visible = true,
     ){
-
+        val listState = rememberTransformingLazyColumnState()
+        val transformationSpec = rememberTransformationSpec()
+        ScreenScaffold(
+            scrollState = listState,
+            edgeButton = {
+                EdgeButton(
+                    onClick = { /*TODO*/ },
+                    colors =
+                        ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                            contentColor = MaterialTheme.colorScheme.onSecondaryContainer,
+                        ),
+                ) {
+                    Text("More")
+                }
+            },
+        ) { contentPadding -> // ScreenScaffold provides default padding; adjust as needed
+            TransformingLazyColumn(contentPadding = contentPadding, state = listState) {}
+        }
     }
     // todo 二楼菜单
     //  联系人

ae2bae8cc471475590d11a99ece2aaa6a1f0234a
Kauid323 <yuzi6666@outlook.com>
2026-06-06 20:13:35 +0800

    修复minsdk不同导致live,player构建炸

--- live/build.gradle.kts
+++ live/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.yhchat.canary.live"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         minSdk = 23
--- player/build.gradle.kts
+++ player/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.yhchat.canary.player"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         minSdk = 23

212678e99fd9dcd99a08cee40ee17e5e7eb354d5
Kauid323 <yuzi6666@outlook.com>
2026-06-20 23:03:47 +0800

    fix

--- app/src/main/java/com/yhchat/canary/ui/chat/ChatComponents/ChatTopAppBar.kt
+++ app/src/main/java/com/yhchat/canary/ui/chat/ChatComponents/ChatTopAppBar.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.RoundedCornerShape
